ac7f2fd4d558bc418e9a6eee1f0f39f43b0d3ff7
Erik <erikmosterud@gmail.com>
2015-05-22 00:26:58 -0400

    Initial commit
    Initial commit

---
+++ FTPUploader/frmMain.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.website = New System.Windows.Forms.LinkLabel()
+        Me.notification = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(55, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(113, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Drop images to upload"
+        '
+        'website
+        '
+        Me.website.AutoSize = True
+        Me.website.Location = New System.Drawing.Point(64, 48)
+        Me.website.Name = "website"
+        Me.website.Size = New System.Drawing.Size(94, 13)
+        Me.website.TabIndex = 1
+        Me.website.TabStop = True
+        Me.website.Text = "View online gallery"
+        '
+        'notification
+        '
+        Me.notification.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info
+        Me.notification.Icon = CType(resources.GetObject("notification.Icon"), System.Drawing.Icon)
+        '
+        'frmMain
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(222, 70)
+        Me.Controls.Add(Me.website)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Location = New System.Drawing.Point(10, 10)
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "FTP Uploader"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents website As System.Windows.Forms.LinkLabel
+    Friend WithEvents notification As System.Windows.Forms.NotifyIcon
+
+End Class
---
+++ FTPUploader/frmMain.vb
@@ -0,0 +1,46 @@
+﻿Public Class frmMain
+
+    Private Sub uploadFiles(ByVal filePath As String, ByVal fileName As String) 'Upload files to the FTP server
+        Dim userName As String = My.Settings.Username
+        Dim password As String = My.Settings.Password
+        Dim ftpServer As String = My.Settings.FTPHost & fileName
+        Me.Cursor = Cursors.WaitCursor
+        My.Computer.Network.UploadFile(filePath, ftpServer, userName, password, True, 500)
+        Me.Cursor = Cursors.Default
+    End Sub
+
+    Private Sub frmMain_DragEnter(ByVal sender As Object, ByVal e As DragEventArgs) Handles Me.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        Else
+            e.Effect = DragDropEffects.None
+        End If
+    End Sub
+
+    Private Sub frmMain_DragDrop(ByVal sender As Object, ByVal e As DragEventArgs) Handles Me.DragDrop
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            Dim filePaths As String() = CType(e.Data.GetData(DataFormats.FileDrop), String())
+            For Each fileLoc As String In filePaths
+                Dim fileName As String = System.IO.Path.GetFileName(fileLoc)
+                Dim fileExt As String = System.IO.Path.GetExtension(fileLoc)
+                Dim tempName = Guid.NewGuid.ToString & fileExt 'Create temporary file name
+                My.Computer.FileSystem.CopyFile(fileLoc, tempName)
+                uploadFiles(fileLoc, tempName)
+                setClipboardText(tempName)
+                My.Computer.FileSystem.DeleteFile(tempName)
+            Next fileLoc
+        End If
+    End Sub
+
+    Private Sub website_LinkClicked(sender As System.Object, e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles website.LinkClicked
+        System.Diagnostics.Process.Start(My.Settings.Domain)
+    End Sub
+
+    Private Sub setClipboardText(ByVal fileName As String)
+        Dim text As String = My.Settings.Domain & "/img/" & fileName
+        Clipboard.SetText(text)
+        notification.Visible = True
+        notification.ShowBalloonTip(3000, "Copied to clipboard...", text, ToolTipIcon.Info)
+    End Sub
+
+End Class
